b28a913e19a92bd2542c446c9b96e3771c055118
Nina Kuhlen <ninakn78@gmail.com>
2025-01-09 15:57:31 +0100

    feat: add confusion matrix

--- src/visualization.py
+++ src/visualization.py
@@ -3,6 +3,7 @@ import matplotlib.pyplot as plt
 from pandas import DataFrame
 from math import ceil
 import numpy as np
+from sklearn.metrics import confusion_matrix, ConfusionMatrixDisplay
 
 
 def plot_columns_as_subplots(data_frame: DataFrame, columns: list, dpi=100):
@@ -128,4 +129,21 @@ def plot_training_history(history, metrics=["loss", "accuracy", "precision", "re
     plt.title("Training and Validation")
     plt.legend()
     plt.grid(True)
+    plt.show()
+
+def plot_confusion_matrix(true_labels, predicted_labels, class_names):
+    """
+    Plots a confusion matrix using matplotlib.
+
+    Args:
+        true_labels (array-like): The true labels (ground truth) from the dataset.
+        predicted_labels (array-like): The predicted labels from the model.
+        class_names (list): List of class names for the matrix.
+    """
+    cm = confusion_matrix(true_labels, predicted_labels)
+    disp = ConfusionMatrixDisplay(confusion_matrix=cm, display_labels=class_names)
+
+    plt.figure(figsize=(8, 8))
+    disp.plot(cmap="Blues", values_format='d')
+    plt.title("Confusion Matrix")
     plt.show()
