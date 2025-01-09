1a469576445958bf7b7477549eeba19fbfe1fff5
Nina Kuhlen <ninakn78@gmail.com>
2025-01-09 13:21:34 +0100

    feat: add visualization for training course

--- src/visualization.py
+++ src/visualization.py
@@ -106,3 +106,26 @@ def plot_fft_spectrogram(data_frame: DataFrame, aspect: str=None, cmap='viridis'
     plt.title('FFT of Vibration Data - Transformed Test')
 
     plt.show()
+
+def plot_training_history(history, metrics=["loss", "accuracy", "precision", "recall"]):
+    """
+    Visualizes the metrics of the training.
+
+    Args:
+        history: The History-Object from Training (model.fit).
+        metrics (list): List of the metrics that should be plotted. Default: loss, accuracy, precision, recall.
+    """
+    plt.figure(figsize=(12, 8))
+    
+    for metric in metrics:
+        if metric in history.history:
+            plt.plot(history.history[metric], label=f"Train {metric}")
+            if f"val_{metric}" in history.history:
+                plt.plot(history.history[f"val_{metric}"], label=f"Validation {metric}")
+    
+    plt.xlabel("Epochs")
+    plt.ylabel("Value")
+    plt.title("Training and Validation")
+    plt.legend()
+    plt.grid(True)
+    plt.show()
